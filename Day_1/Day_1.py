fc7908a6bbcd9b3b1ed9c7cc136ac84686a342a2
Hassim Mazahere Rakotomalala <hassim.rakotomalala@shadow.tech>
2022-12-01 22:24:17 +0100

    fix (Puzzle): cleanup code a bit

--- Day_1/Day_1.py
+++ Day_1/Day_1.py
@@ -12,16 +12,14 @@ def main(inventory: str) -> list:
      (eg: the most loaded elf carries the calories amount in index 0 of the list)
     """
     calories_inventory = []
-    inventory_per_elf = inventory.split("\n\n")
 
-    if (inventory_per_elf):
-        for inventory in inventory_per_elf:
-            total_calories_per_elf = 0
-            items = inventory.split("\n")
-            for item in items:
-                if (item):
-                    total_calories_per_elf = total_calories_per_elf + int(item)
-                calories_inventory.append(total_calories_per_elf)
+    for inv in inventory.split("\n\n"):
+        total_calories_per_elf = 0
+
+        for item in inv.splitlines():
+            total_calories_per_elf += int(item)
+
+        calories_inventory.append(total_calories_per_elf)
 
     calories_inventory.sort(reverse=True)
     return calories_inventory
@@ -30,20 +28,11 @@ def main(inventory: str) -> list:
 if (__name__ == "__main__"):
     ret = None
     input = ""
-    try:
-        with open('input.txt') as f:
-            input = f.read()
-        if (not input):
-            print("Coudn't read input file")
-        else:
-            ret = main(input)
-            if (ret):
-                print("Sum of calories of the 3 most loaded elves")
-                if (len(ret) >= 3):
-                    print(ret[0]+ret[1]+ret[2])
-                else:
-                    print("There isn't 3 elves in this list")
-            else:
-                print("Script probably crashed or result was empty (improbable)")
-    except Exception as e:
-        print(e)
+
+    with open('input.txt') as f:
+        input = f.read()
+        assert input
+
+    ret = main(input)
+    assert (len(ret) >= 3)
+    print(sum(ret[0:3]))
