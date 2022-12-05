67cbbe46836eb54e7be6208b1a6342870836f57b
Hassim Mazahere Rakotomalala <hassim.rakotomalala@shadow.tech>
2022-12-05 01:19:21 +0100

    fix (input path): change input path to work in day folder

--- Day_3/Day_3.py
+++ Day_3/Day_3.py
@@ -74,7 +74,7 @@ def FindBadges(ruck_sack_list: str) -> int:
 if (__name__ == "__main__"):
     ret = None
     input = ""
-    with open('Day_3/input.txt') as f:
+    with open('input.txt') as f:
         input = f.read()
         assert input
 
